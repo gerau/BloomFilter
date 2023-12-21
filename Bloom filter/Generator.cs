6a8ab777e12a7e99ab7d66e9d7e7f5293f91a422
Volodymyr Herashchenko <cholkegn@gmail.com>
2023-12-21 18:32:05 +0200

    added generator and executable

---
+++ Bloom filter/Generator.cs
@@ -0,0 +1,19 @@
+﻿namespace Bloom_filter
+{
+    public class Generator
+    {
+
+        public static string GenerateRandomString(int maxLength)
+        {
+            Random random = new Random();
+            var rand = random.Next(maxLength);
+            var result = "";
+
+            for (int i = 0; i < rand; i++)
+            {
+                result += (char)random.Next(byte.MaxValue);
+            }
+            return result;
+        }
+    }
+}
